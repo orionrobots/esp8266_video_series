6d3b18b831eeb0ce89425f09c50c8d24ba352803
Danny Staple <danny@orionrobots.co.uk>
2017-08-09 11:52:57 +0100

    Spiderbot with Gait tests

---
+++ spiderbot/spiderbot.py
@@ -0,0 +1,65 @@
+"""Control for the spider bot servo's.
+Libraries:
+    * micropython-adafruit-pca9685 - servo.py and pca9685.py.
+    * https://bitbucket.org/thesheep/micropython-servo/src/f562a6abeaf0e83b752838df7cd31d88ea10b2c7/servo.py?at=default&fileviewer=file-view-default
+        * as direct_servo.py with class DServo.
+"""
+from machine import Pin, I2C
+import servo
+from direct_servo import DServo
+
+# Todo: Potentially flatten this with the other classes I have the source for.
+class Servo9686():
+    def __init__(self, adafruit_servos, index):
+        self._servos = adafruit_servos
+        self._index = index
+        self.invert = False
+
+    def position(self, angle):
+        if self.invert:
+            angle = 180 - angle
+        self._servos.position(self._index, angle)
+    
+    def release(self):
+        self._servos.release(self._index)
+
+
+class ServoDirect():
+    def __init__(self, pin):
+        self._servo = DServo(pin)
+        self.invert = False
+
+    def position(self, angle):
+        if self.invert:
+            angle = 180 - angle
+        self._servo.write_angle(angle)
+
+    def release(self):
+        self._servo.write_us(0)
+
+
+class Robot:
+    def __init__(self, i2c):
+        s = servo.Servos(i2c)
+        servo_list = []
+        for n in range(16):
+            servo_list.append(Servo9686(s, n))
+        servo_list.append(ServoDirect(Pin(0)))
+        servo_list.append(ServoDirect(Pin(2)))
+
+        # Set them all to 90 degrees
+        for n in range(18):
+            servo_list[n].position(90)
+        self.servo_list = servo_list
+
+
+class Leg():
+    def __init__(self, robot, hip, knee, foot, invert=False):
+        """Hip, Knee and Foot are Servo classes.
+        Invert will invert angles on hips and knees"""
+        self.hip = robot.servo_list[hip]
+        self.knee = robot.servo_list[knee]
+        self.foot = robot.servo_list[foot]
+        if invert:
+            self.hip.invert = True
+            self.knee.invert = True
